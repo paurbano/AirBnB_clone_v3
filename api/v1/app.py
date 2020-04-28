1adcf95d2a4597bd100086b7d69bf948feaa3c44
elisa2105 <aikomi1726@gmail.com>
2020-04-28 21:39:52 +0000

    pep8 compliant

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,11 +8,13 @@ from os import getenv
 app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix='/api/v1')
 
+
 @app.errorhandler(404)
 def error_404(error):
     """ return 404 error """
     errorF = {"error": "Not found"}
     return jsonify(errorF), 404
+
 
 @app.teardown_appcontext
 def teardown(self):
