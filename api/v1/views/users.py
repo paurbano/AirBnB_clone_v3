1fec0138a255e4bef13167476d124d72adfb659a
paurbano <paurbano@gmail.com>
2020-04-29 06:12:49 +0000

    fix imports and naming vriables

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -4,6 +4,7 @@ from models import storage
 from api.v1.views import app_views
 from flask import jsonify, abort, make_response
 from flask import request
+from models.user import User
 
 
 @app_views.route('/users',  methods=['GET'], strict_slashes=False)
